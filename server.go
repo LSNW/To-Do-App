31b02a544fa0b0ef8668fafb08d46a4ac54bd00a
Winston Leung <winstonleung@Winstons-MBP.ht.home>
2022-05-12 15:30:35 -0400

    feat: added user and todo models

--- server.go
+++ server.go
@@ -1,13 +1,46 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+  	"gorm.io/driver/postgres"
+ 	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	login string
+	password string
+}
+
+type ToDo struct {
+	gorm.Model
+	task string
+	assignment string
+	status string
+	delete string
+}
 
 func main() {
-  app := fiber.New()
+	dsn := "host=localhost user=todo password=todopassword dbname=todo port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
-  app.Get("/", func(c *fiber.Ctx) error {
-    return c.SendString("Hello, World!")
-  })
+	if err != nil {
+		panic("")
+	}
+	db.AutoMigrate(&User{})
+	db.AutoMigrate(&ToDo{})
+	
+	db.Create(&User{login: "john", password: "john_password"})
+	db.Create(&ToDo{task: "exTask", assignment: "exAssignment", status:"exStatus", delete:"exDelete"})
 
-  app.Listen(":3000")
+	var todo ToDo
+	db.First(&todo, 1)
+
+  	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+    	return fiber.NewError(782, "Custom error message")
+	})
+
+  	app.Listen(":3000")
 }
