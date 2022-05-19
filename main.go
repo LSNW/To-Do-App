3c9ad41ad56980a12d7cdbdd337a9eea1b5d53ed
Winston Leung <52609843+LSNW@users.noreply.github.com>
2022-05-19 09:05:01 -0400

    7-add-sessions-build-auth-service (#11)
    feat: added cookies/sessions

---
+++ main.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+  	"gorm.io/driver/postgres"
+	"github.com/gofiber/storage/redis"
+	"gorm.io/gorm"
+	"strconv"
+	"github.com/gofiber/fiber/v2/middleware/session"
+	"time"
+	//"fmt"
+)
+
+type User struct {
+	gorm.Model
+	Login string `json:"login"`
+	Password string `json:"password"`
+}
+
+type ToDo struct {
+	gorm.Model
+	Task string `json:"task"`
+	Assignment string `json:"assignment"`
+	Status string `json:"status"`
+	Delete string `json:"delete"`
+}
+
+func main() {
+	// app, database, cache initializations
+	dsn := "host=localhost user=todo password=todopassword dbname=todo port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("")
+	}
+	db.AutoMigrate(&User{}, &ToDo{})
+  	app := fiber.New()
+	rdb := redis.New(redis.Config{
+		Port: 6379,
+	})
+	store := session.New(session.Config{
+		Storage: rdb,
+	})
+
+	expirationTime := 7 * time.Second
+
+	resetCookieExpiration := func(c *fiber.Ctx) {
+		cookie := new(fiber.Cookie)
+		cookie.Name = "session_id"
+		cookie.Value = c.Cookies("session_id")
+		cookie.Expires = time.Now().Add(expirationTime)
+		c.Cookie(cookie)
+	}
+
+	// this is a proper login
+	app.Post("/login", func(c *fiber.Ctx) error {
+		var attemptedUser User
+		var existsUser User
+
+		if err := c.BodyParser(&attemptedUser); err != nil {
+			return err
+		}
+		result := db.Last(&existsUser, "login = ?", attemptedUser.Login)
+		// separation is for debug
+		if attemptedUser.Login == "" {
+			return c.SendString("Please enter a login")
+		} else if result.RowsAffected == 0  {
+			return c.SendString("Incorrect login")
+		} else if attemptedUser.Password != existsUser.Password {
+			return c.SendString("Incorrect password")
+		}
+
+		sess, err := store.Get(c)
+		if err != nil {
+			panic(err)
+		}
+		sess.Regenerate()
+		defer func() {
+			sess.SetExpiry(expirationTime)
+			if err := sess.Save(); err != nil {
+				panic(err)
+			}
+		}()
+
+		return c.SendString(existsUser.Login +  ", you are now logged in for 10 seconds")
+	})
+
+	// this is an automatic login
+	app.Get("/login", func(c *fiber.Ctx) error {
+		sess, err := store.Get(c)
+		if err != nil {
+			panic(err)
+		}
+		sess.Regenerate()
+		defer func() {
+			sess.SetExpiry(expirationTime)
+			if err := sess.Save(); err != nil {
+				panic(err)
+			}
+		}()
+
+		return c.SendString("You are now \"logged in\" for 10 seconds")
+	})
+
+	// session tester (landing page)
+	app.Get("/", func(c *fiber.Ctx) error {
+		sess, err := store.Get(c)
+		if err != nil {
+			panic(err)
+		}
+		if c.Cookies("session_id") != sess.ID() {
+			return c.SendStatus(401)
+		}
+		resetCookieExpiration(c)
+		return c.SendString("Authorized :D")
+	})
+
+
+	// ToDo REST API, does not require login
+	app.Post("/api/ToDo/", func(c *fiber.Ctx) error {
+		var todo ToDo
+
+		if err := c.BodyParser(&todo); err != nil {
+			return err
+		}
+
+		db.Create(&ToDo{Task: todo.Task, Assignment: todo.Assignment, Status: todo.Status, Delete: todo.Delete})
+		return c.SendString("Successfully created")
+	})
+	
+	app.Get("/api/ToDo/:task", func(c *fiber.Ctx) error {
+		var todo ToDo
+		result :=  db.Last(&todo, "task = ?", c.Params("task"))
+
+		if result.RowsAffected == 0 {
+			return c.SendStatus(404)
+		}
+		
+		return c.Status(200).JSON(todo)
+  	})
+
+	app.Patch("/api/ToDo/:task", func(c *fiber.Ctx) error {
+		var todo ToDo
+		var updatedToDo ToDo
+
+		db.Last(&todo, "task = ?", c.Params("task"))
+		if err := c.BodyParser(&updatedToDo); err != nil {
+			return err
+		}
+
+		if updatedToDo.Task != "" {
+			todo.Task = updatedToDo.Task
+		}
+		if updatedToDo.Assignment != "" {
+			todo.Assignment = updatedToDo.Assignment
+		}
+		if updatedToDo.Status != "" {
+			todo.Status = updatedToDo.Status
+		}
+		if updatedToDo.Delete != "" {
+			todo.Delete = updatedToDo.Delete
+		}
+
+		db.Save(&todo)
+		return c.SendString("Successfully updated")
+	})
+
+	app.Delete("/api/ToDo/:task", func(c *fiber.Ctx) error {
+		var todo ToDo
+		result := db.Where("task = ?", c.Params("task")).Delete(&todo)
+		//return c.Status(200).JSON(todo)
+		return c.SendString("Deleted " + strconv.Itoa(int(result.RowsAffected)) + " entries with " + c.Params("task"))
+  	})
+
+
+  	app.Listen(":3000")
+}
