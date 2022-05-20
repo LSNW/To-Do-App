1b1fe26de4b48725f7180d948cc05a9a45c536c5
Winston Leung <winstonl2002@gmail.com>
2022-05-20 15:19:46 -0400

    refactor: establish a one to many relationship between user and todos

--- main.go
+++ main.go
@@ -10,9 +10,9 @@ import (
 	"time"
 	"github.com/alexedwards/argon2id"
 	"github.com/jinzhu/copier"
+	//"fmt
 
 	"ToDoApp/app/models"
-	//"fmt"
 )
 
 
@@ -32,7 +32,7 @@ func main() {
 		Storage: rdb,
 	})
 
-	expirationTime := 7 * time.Second
+	expirationTime := 15 * time.Minute
 
 	resetCookieExpiration := func(c *fiber.Ctx) {
 		cookie := new(fiber.Cookie)
@@ -41,15 +41,6 @@ func main() {
 		cookie.Expires = time.Now().Add(expirationTime)
 		c.Cookie(cookie)
 	}
-
-	app.Get("/copytest/:user", func(c *fiber.Ctx) error {
-		var user models.User
-		var userResponse models.UserResponse
-		db.Last(&user, "login = ?", c.Params("user"))
-		copier.Copy(&userResponse, &user)
-
-		return c.Status(200).JSON(userResponse)
-	})
 
 	// this is a proper login
 	app.Post("/login", func(c *fiber.Ctx) error {
@@ -72,19 +63,21 @@ func main() {
 			return c.SendString("Incorrect login/password")
 		}
 
+		var responseUser models.UserResponse
+		copier.Copy(&responseUser, &existsUser)
 		sess, err := store.Get(c)
 		if err != nil {
 			panic(err)
 		}
 		sess.Regenerate()
 		defer func() {
-			sess.SetExpiry(expirationTime)
+			sess.Set("user_id", responseUser.ID)
 			if err := sess.Save(); err != nil {
 				panic(err)
 			}
 		}()
 
-		return c.SendString(existsUser.Login +  ", you are now logged in for 7 seconds")
+		return c.SendString(existsUser.Login +  ", you are now logged in")
 	})
 
 	// this is an automatic login
@@ -95,16 +88,16 @@ func main() {
 		}
 		sess.Regenerate()
 		defer func() {
-			sess.SetExpiry(expirationTime)
+			sess.Set("user_id", 154) // 154 is just a random user id
 			if err := sess.Save(); err != nil {
 				panic(err)
 			}
 		}()
 
-		return c.SendString("You are now \"logged in\" for 7 seconds")
+		return c.SendString("You are now \"logged in\"")
 	})
 
-	// session tester (landing page)
+	// this landing page is for testing session authenticity
 	app.Get("/", func(c *fiber.Ctx) error {
 		sess, err := store.Get(c)
 		if err != nil {
@@ -114,7 +107,7 @@ func main() {
 			return c.SendStatus(401)
 		}
 		resetCookieExpiration(c)
-		return c.SendString("Welcome")
+		return c.SendString("Welcome, your user ID is " +  strconv.Itoa(int(sess.Get("user_id").(uint))))
 	})
 
 	// User REST API
@@ -138,37 +131,84 @@ func main() {
 		return c.SendString("Successfully created user profile for  " + user.Login)
 	})
 
+	app.Get("/api/user/:user", func(c *fiber.Ctx) error {
+		var user models.User
+		var userResponse models.UserResponse
+		result := db.Last(&user, "login = ?", c.Params("user"))
+		if result.RowsAffected == 0 {
+			return c.SendStatus(404)
+		}
+		copier.Copy(&userResponse, &user)
 
-	// ToDo REST API, does not require login
+		return c.Status(200).JSON(userResponse)
+	})
+
+
+	// ToDo REST API
 	app.Post("/api/ToDo/", func(c *fiber.Ctx) error {
 		var todo models.ToDo
+		sess, err := store.Get(c)
+		if err != nil {
+			panic(err)
+		} else if c.Cookies("session_id") != sess.ID() {
+			return c.SendStatus(401)
+		}
 
 		if err := c.BodyParser(&todo); err != nil {
 			return err
 		}
-
-		db.Create(&models.ToDo{Task: todo.Task, Assignment: todo.Assignment, Status: todo.Status, Delete: todo.Delete})
-		return c.SendString("Successfully created")
+		createToDo := models.ToDo{Task: todo.Task, Assignment: todo.Assignment, 
+			Status: todo.Status, Delete: todo.Delete, UserID: sess.Get("user_id").(uint)}
+		db.Create(&createToDo)
+		return c.Status(200).JSON(createToDo)
 	})
 	
-	app.Get("/api/ToDo/:task", func(c *fiber.Ctx) error {
-		var todo models.ToDo
-		result :=  db.Last(&todo, "task = ?", c.Params("task"))
+	app.Get("/api/ToDo/all", func(c *fiber.Ctx) error {
+		var todos []models.ToDo
+		sess, err := store.Get(c)
+		if err != nil {
+			panic(err)
+		} else if c.Cookies("session_id") != sess.ID() {
+			return c.SendStatus(401)
+		}
+		db.Where(&models.ToDo{UserID: sess.Get("user_id").(uint)}).Find(&todos)
+		
+		return c.Status(200).JSON(todos)
+  	})
 
-		if result.RowsAffected == 0 {
+	app.Get("/api/ToDo/:id", func(c *fiber.Ctx) error {
+		var todo models.ToDo
+		sess, err := store.Get(c)
+
+		result :=  db.Last(&todo, c.Params("id"))
+
+		if err != nil {
+			panic(err)
+		} else if c.Cookies("session_id") != sess.ID() || todo.UserID != sess.Get("user_id").(uint) {
+			return c.SendStatus(401)
+		} else if result.RowsAffected == 0 {
 			return c.SendStatus(404)
 		}
 		
 		return c.Status(200).JSON(todo)
   	})
 
-	app.Patch("/api/ToDo/:task", func(c *fiber.Ctx) error {
+	app.Patch("/api/ToDo/:id", func(c *fiber.Ctx) error {
 		var todo models.ToDo
 		var updatedToDo models.ToDo
+		sess, err := store.Get(c)
+		if err != nil {
+			return err
+		}
 
-		db.Last(&todo, "task = ?", c.Params("task"))
+		result := db.Last(&todo, c.Params("id"))
+
 		if err := c.BodyParser(&updatedToDo); err != nil {
 			return err
+		} else if c.Cookies("session_id") != sess.ID() || todo.UserID != sess.Get("user_id").(uint) {
+			return c.SendStatus(401)
+		} else if result.RowsAffected == 0 {
+			return c.SendStatus(404)
 		}
 
 		if updatedToDo.Task != "" {
@@ -188,11 +228,19 @@ func main() {
 		return c.SendString("Successfully updated")
 	})
 
-	app.Delete("/api/ToDo/:task", func(c *fiber.Ctx) error {
+	app.Delete("/api/ToDo/:id", func(c *fiber.Ctx) error {
 		var todo models.ToDo
-		result := db.Where("task = ?", c.Params("task")).Delete(&todo)
-		//return c.Status(200).JSON(todo)
-		return c.SendString("Deleted " + strconv.Itoa(int(result.RowsAffected)) + " entries with " + c.Params("task"))
+		sess, err := store.Get(c)
+		if err != nil {
+			return err
+		} else if c.Cookies("session_id") != sess.ID() || todo.UserID != sess.Get("user_id").(uint) {
+			return c.SendStatus(401)
+		}
+		result := db.First(&todo, c.Params("task")).Delete(&todo)
+		if result.RowsAffected == 0 {
+			return c.SendStatus(404)
+		}
+		return c.SendString("Task has been deleted")
   	})
 
 
